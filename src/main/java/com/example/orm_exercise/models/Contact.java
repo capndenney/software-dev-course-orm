edf5871a98812515350bb769ca42d728a55f2f9c
Mike Denney <mike.a.denney@gmail.com>
2026-02-02 20:08:45 -0600

    group work completed

--- src/main/java/com/example/orm_exercise/models/Contact.java
+++ src/main/java/com/example/orm_exercise/models/Contact.java
@@ -1,9 +1,9 @@
 package com.example.orm_exercise.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Contact {
@@ -13,6 +13,9 @@ public class Contact {
     private String name;
     private String email;
     private String phoneNumber;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "contact", orphanRemoval = true)
+    @JsonManagedReference
+    private List<Address> addresses;
 
     public int getId() {
         return id;
@@ -46,10 +49,36 @@ public class Contact {
         this.phoneNumber = phoneNumber;
     }
 
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void addAddress(Address address) {
+        if (this.addresses == null) {
+            this.addresses = new java.util.ArrayList<>();
+        }
+        this.addresses.add(address);
+        address.setContact(this);
+    }
+
+//    public void setAddresses(List<Address> addresses) {
+//        this.addresses = addresses;
+//    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+        if (addresses != null) {
+            for (Address address : addresses) {
+                address.setContact(this); // The "Smoking Gun" fix
+            }
+        }
+    }
+
     public Contact() {
     }
 
     public Contact(int id, String name, String email, String phoneNumber) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
